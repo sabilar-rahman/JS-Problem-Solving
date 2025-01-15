3ef1913c01d81e7bc5e3a11f0bc3d54207a23d71
sabilar-rahman <sabilarrahman36@gmail.com>
2025-01-15 12:51:26 +0600

    arithmetic

---
+++ arithmatic.js
@@ -0,0 +1,10 @@
+console.log(10%3)// remainder
+console.log(10/3)// quotient
+
+var a=10;
+var b=20;
+console.log(a+b); //addition
+console.log(a-b); //subtraction
+console.log(a*b); //multiplication
+console.log(a/b); //division
+console.log(a%b); //remainder
